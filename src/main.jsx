0b276e24b7b4c7df6112707716f465d8818d8d15
Angie <angel890817@hotmail.com>
2025-05-10 10:56:03 +1000

    feat Basic Routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { PokemonSearcher } from './component/PokemonSercher.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PokemonSearcher />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello, HomePage</h1>}/>
+        <Route path="/about" element={<h1>Hello, aboutPage</h1>}/>
+        <Route path="/pokemon" element={<PokemonSearcher/>}/>
+
+        
+      </Routes>
+    </BrowserRouter>,
   </StrictMode>,
 )
