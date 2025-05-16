4bf472bb329557b15164faaa2063567630feb0ef
Angie <angel890817@hotmail.com>
2025-05-16 13:56:54 +1000

    feat testing user input

--- tests/PokemonSearcher.test.jsx
+++ tests/PokemonSearcher.test.jsx
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { PokemonSearcher } from "../src/component/PokemonSercher";
+import userEvent from "@testing-library/user-event";
 
 
 
@@ -19,5 +20,35 @@ describe("PokemonSearcher component test", ()=> {
     test.skip("Pokemon Searcher makes a random number", () => {
 
     });
+
+    test("Can type in pokemon name and search for that provided name", async() => {
+
+        const result = render(<PokemonSearcher/>);
+
+        //Find the text input field for the pokemon name
+        const pokemonSearchNameInputElement = screen.getByTestId("pokemonNameInput");
+        expect(pokemonSearchNameInputElement).toBeInTheDocument();
+
+        const user = userEvent.setup();
+
+        // await user.click(pokemonSearchNameInputElement);
+        // await user.keyboard("pikachu");
+        await user.type(pokemonSearchNameInputElement, "pikachu");
+
+
+        // expect(pokemonSearchNameInputElement.innerText).toBe("pikachu");
+        waitFor(()=>{
+            expect(pokemonSearchNameInputElement.value).toBe("pikachu");
+
+        })
+        
+
+
+        // Find the button to submit the pokemon name to the API
+
+        // Find the content render to the page to reflect the API result
+
+
+    })
 
 });
